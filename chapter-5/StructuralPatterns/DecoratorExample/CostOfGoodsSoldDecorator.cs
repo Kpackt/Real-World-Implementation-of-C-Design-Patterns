a418ef01a3f2d1955918260ed5a86ca10539b0ee
Bruce Van Horn <brucevanhorn@me.com>
2022-05-10 22:12:51 -0500

    time for bed.  i'm kind of stuck here.  maybe tomorrow?

---
+++ chapter-5/StructuralPatterns/DecoratorExample/CostOfGoodsSoldDecorator.cs
@@ -0,0 +1,32 @@
+﻿using BumbleBikesLibrary;
+
+namespace DecoratorExample;
+
+public class CostOfGoodsSoldDecorator : CostDecorator
+{
+    public CostOfGoodsSoldDecorator(IBicycle bicycle) : base(bicycle)
+    {
+        
+    }
+
+    public Decimal GetCost()
+    {
+        switch (UndecoratedBicycle.ModelName)
+        {
+            case "Hillcrest":
+                return 1231.09m;
+                break;
+            case "Palo Duro Canyon Ranger":
+                return 1832.09m;
+                break;
+            case "Galveston Cruiser":
+                return 425.38m;
+                break;
+            case "Big Bend":
+                return 739.32m;
+                break;
+            default:
+                return 0m;
+        }
+    }
+}
