9cd42875d5585ca583be0fa7be633b8c51edeeda
Bruce Van Horn <brucevanhorn@me.com>
2022-05-13 22:58:59 -0500

    Added colors to the decorators so you can see the stacked one run really well.  Somehow forgot to add a placeholder for facade.

--- chapter-4/DecoratorExample/ManualPrinter.cs
+++ chapter-4/DecoratorExample/ManualPrinter.cs
@@ -4,6 +4,8 @@ public class ManualPrinter : IDocumentor
 {
     public void PrintManual()
     {
+        Console.ForegroundColor = ConsoleColor.Cyan;  // so it stands out
         Console.WriteLine("The manual is printing!");
+        Console.ResetColor();
     }
 }
--- chapter-4/DecoratorExample/MaterialsInventoryNotifier.cs
+++ chapter-4/DecoratorExample/MaterialsInventoryNotifier.cs
@@ -4,6 +4,8 @@ public class MaterialsInventoryNotifier : INotifier
 {
     public void Notify()
     {
+        Console.ForegroundColor = ConsoleColor.Yellow; // so it stands out
         Console.WriteLine("The materials inventory control system has been notified regarding the manufacture of this bicycle.");
+        Console.ResetColor();
     }
 }
