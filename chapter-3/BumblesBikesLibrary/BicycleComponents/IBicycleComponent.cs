4fc9faad5cc259c428f7cf06c35e5d0cb44f6e38
Bruce Van Horn <brucevanhorn@me.com>
2022-05-14 21:13:37 -0500

    add component interface

---
+++ chapter-3/BumblesBikesLibrary/BicycleComponents/IBicycleComponent.cs
@@ -0,0 +1,8 @@
+﻿namespace BumblesBikesLibrary.BicycleComponents;
+
+public interface IBicycleComponent
+{
+    public string Name { get; set; }
+    public float Weight { get; set; }
+    public float Cost { get; set; }
+}
