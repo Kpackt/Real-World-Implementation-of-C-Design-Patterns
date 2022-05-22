069b8f279bb137f97370ae533fb9516b179a2c5d
Bruce Van Horn <brucevanhorn@me.com>
2022-05-22 09:07:23 -0500

    added paintable bicycle interface

---
+++ chapter-3/BumbleBikesLibrary/IPaintableBicycle.cs
@@ -0,0 +1,15 @@
+﻿namespace BumbleBikesLibrary;
+
+// same as IBicycle but with paint color enum removed
+// see the chapter on the Bridge pattern
+public interface IPaintableBicycle
+{
+    public string ModelName { get; set; }
+    public int Year { get; }
+    public string SerialNumber { get; }
+    public BicycleGeometries Geometry { get; set; }
+    public SuspensionTypes Suspension { get; set; }
+    public ManufacturingStatus BuildStatus { get; set; }
+
+    public void Build();
+}
