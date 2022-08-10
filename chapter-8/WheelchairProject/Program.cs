ecc10880848c04482c37fd2593ad32b9adf59caa
Bruce Van Horn <brucevanhorn@me.com>
2022-08-09 20:47:56 -0500

    converted director class into a singleton

--- chapter-8/WheelchairProject/Program.cs
+++ chapter-8/WheelchairProject/Program.cs
@@ -4,9 +4,9 @@ using WheelchairProject.Builders;
 
 Console.WriteLine("Hello, World!  Let's make some Plano Wheelchairs");
 
-var builder = new PlanoWheelchairBuilder();
-var director = new WheelchairBuilderDirector(builder);
-
-var planoWheelchair = director.Build();
-planoWheelchair.DisplayCost();
-planoWheelchair.DisplayWeight();
+// var builder = new PlanoWheelchairBuilder();
+// var director = new WheelchairBuilderDirector(builder);
+//
+// var planoWheelchair = director.Build();
+// planoWheelchair.DisplayCost();
+// planoWheelchair.DisplayWeight();
