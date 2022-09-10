f1ac0c85dd62d3be8ec4af8af084bd95e0e869b2
Bruce Van Horn <brucevanhorn@me.com>
2022-09-10 16:14:12 -0500

    removed unused code

--- chapter-6/MobilityLibrary/IMobilityDevice.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MobilityLibrary;
-
-public interface IMobilityDevice
-{
-    public string ModelName { get; set; }
-    public int Year { get; set; }
-    public string SerialNumber { get; }
-    
-}
